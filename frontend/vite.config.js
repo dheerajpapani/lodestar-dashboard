cd912511f922550dd145149ce7be9d20dd9eb8bb
dheerajpapani <dheerajpapani@gmail.com>
2025-07-16 21:06:38 +0530

    Update package.json and vite.config.js for project configuration and deployment settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   envDir: '../', 
   plugins: [react()],
-})
+  base: '/lodestar-dashboard/',  // Set the base path for GitHub Pages
+  build: {
+    outDir: 'dist',  // Ensure the build output is placed into the 'dist' folder
+  },
+});
